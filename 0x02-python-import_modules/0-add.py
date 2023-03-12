5fa0486e2f12c789f0495b7edfa10e903797b6c1
Yusif-Hamdaani <yusif.muhammed1989@gmail.com>
2023-03-12 11:15:56 -0700

    All tasks on Import & modules

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -2,6 +2,5 @@
 if __name__ == "__main__":
     from add_0 import add
 
-    a = 1
-    b = 2
-    print('{:d} + {:d} = {:d}'.format(a, b, add(a, b))i)
+    result = add(1, 2)
+    print("1 + 2 = ", result)
