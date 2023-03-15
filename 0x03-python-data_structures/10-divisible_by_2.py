c5589509e77d68f6b191e0808b66e07990ca95c2
Yusif-Hamdaani <yusif.muhammed1989@gmail.com>
2023-03-15 08:30:29 -0700

    All tasks

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,16 +1,13 @@
 #!/usr/bin/python3
 
-def divisible_by_2(my_list=[]):
-    # Create an empty list to store the results
-    results = []
+#!/usr/bin/python3
 
-    # Loop through each element in the input list
-    for num in my_list:
-        # Check if the element is divisible by 2
-        if num % 2 == 0:
-            results.append(True)
+def divisible_by_2(my_list=[]):
+    new_list = []
+
+    for i in my_list:
+        if i % 2 == 0:
+            new_list.append(True)
         else:
-            results.append(False)
-    
-    # Return the list of results
-    return results
+            new_list.append(False)
+    return (new_list)
