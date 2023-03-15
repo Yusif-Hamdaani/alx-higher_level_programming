a7d6cbbf5f8dd491c40cd65000cd24bd34335579
Yusif-Hamdaani <yusif.muhammed1989@gmail.com>
2023-03-15 07:25:29 -0700

    All tasks

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -4,7 +4,7 @@ def add_tuple(tuple_a=(), tuple_b=()):
     # Get the first element of each tuple, or 0 if the tuple is too small
     a1 = tuple_a[0] if len(tuple_a) >= 1 else 0
     b1 = tuple_b[0] if len(tuple_b) >= 1 else 0
-    
+
     # Get the second element of each tuple, or 0 if the tuple is too small
     a2 = tuple_a[1] if len(tuple_a) >= 2 else 0
     b2 = tuple_b[1] if len(tuple_b) >= 2 else 0
