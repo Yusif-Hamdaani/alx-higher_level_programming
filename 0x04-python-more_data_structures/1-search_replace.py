e810e93f8f91b5a3897c237f2f477bd314006b36
Yusif-Hamdaani <yusif.muhammed1989@gmail.com>
2023-03-16 23:32:12 -0700

    This about More Data Structures: Set, Dictionary in Python

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,10 +1,5 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    """Returns a new list where all occurrences of search in my_list are replaced by replace."""
-    new_list = []
-    for elem in my_list:
-        if elem == search:
-            new_list.append(replace)
-        else:
-            new_list.append(elem)
-    return new_list
+    def find_search(element):
+        return element if element != search else replace
+    return list(map(find_search, my_list))
